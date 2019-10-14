8e91d4dbd30aaa320c1117ecb7f31b80a30983d2
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-10-14 18:42:54 +0700

    update

--- application/src/Main.java
+++ application/src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +12,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
 
+            Parent main = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
+            Scene scene = new Scene(main);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
