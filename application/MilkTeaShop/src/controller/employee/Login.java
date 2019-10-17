1ec7cd74f873799bdc7e49140d80a22c99266851
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-10-17 23:16:54 +0700

    update

--- application/MilkTeaShop/src/controller/employee/Login.java
+++ application/MilkTeaShop/src/controller/employee/Login.java
@@ -2,9 +2,9 @@ package controller.employee;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.Main;
 import model.Account;
 import ui.ErrorAlert;
 import ui.WarningAlert;
@@ -12,28 +12,31 @@ import ui.WarningAlert;
 import java.io.IOException;
 
 public class Login extends controller.Login {
-
     @Override
     public void login(ActionEvent event) {
-        Account account = access.Account.getInstance().login(getName(), getPassword());
-        if (account == null) {
-            WarningAlert.getInstance().showAndWait("Login failed");
-        } else {
-            try {
-                // Create stageCustomer with Scene viewWelcome
-                Stage secondaryStage = new Stage();
-                FXMLLoader view = new FXMLLoader(this.getClass().getResource("/view/customer/Welcome.fxml"));
-                secondaryStage.setTitle("Milk Tea Shop");
-                secondaryStage.setScene(new Scene(view.load()));
-                secondaryStage.setResizable(false);
-                secondaryStage.show();
+        if (getName().matches("^\\w{2,50}$") && getPassword().matches("^\\w{1,50}$")) {
+            Account account = access.Account.getInstance().login(getName(), getPassword());
+            if (account != null) {
+                try {
+                    // Set primary stage for employee
+                    FXMLLoader primaryView = new FXMLLoader(this.getClass().getResource("/view/employee/Main.fxml"));
+                    Main.getPrimaryStage().setScene(new Scene(primaryView.load()));
 
-
-                //Change Scene viewEmployee on stageEmployee
-                Stage stageEmployee = (Stage) (((Node) event.getSource()).getScene().getWindow());
-            } catch (IOException e) {
-                ErrorAlert.getInstance().showAndWait(e);
+                    // Set secondary stage for customer
+                    FXMLLoader secondaryView = new FXMLLoader(this.getClass().getResource("/view/customer/Welcome.fxml"));
+                    Main.setSecondaryStage(new Stage());
+                    Main.getSecondaryStage().setTitle("Milk Tea Shop");
+                    Main.getSecondaryStage().setScene(new Scene(secondaryView.load()));
+                    Main.getSecondaryStage().setResizable(false);
+                    Main.getSecondaryStage().show();
+                } catch (IOException e) {
+                    ErrorAlert.getInstance().showAndWait(e);
+                }
+            } else {
+                WarningAlert.getInstance().showAndWait("Login failed", "Account or password is incorrect");
             }
+        } else {
+            WarningAlert.getInstance().showAndWait("Login failed", "Invalid account");
         }
     }
 }
--- application/MilkTeaShop/src/main/Main.java
+++ application/MilkTeaShop/src/main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import access.DataProvider;
 import controller.employee.Login;
 import javafx.application.Application;
@@ -5,25 +7,44 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ui.ErrorAlert;
 import org.jetbrains.annotations.NotNull;
+import ui.ErrorAlert;
 
 public class Main extends Application {
+    private static Stage primaryStage;
+    private static Stage secondaryStage;
+
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    private static void setPrimaryStage(Stage primaryStage) {
+        Main.primaryStage = primaryStage;
+    }
+
+    public static Stage getSecondaryStage() {
+        return secondaryStage;
+    }
+
+    public static void setSecondaryStage(Stage secondaryStage) {
+        Main.secondaryStage = secondaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(@NotNull Stage primaryStage) {
+    public void start(@NotNull Stage stage) {
+        setPrimaryStage(stage);
         try {
             // Load view and set controller
-            FXMLLoader view = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
-            view.setController(new Login());
+            FXMLLoader primaryView = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
+            primaryView.setController(new Login());
 
             // Set scene for primaryStage
             primaryStage.setTitle("Milk Tea Shop");
-            primaryStage.setScene(new Scene(view.load()));
+            primaryStage.setScene(new Scene(primaryView.load()));
             primaryStage.setResizable(false);
 
             // Set event OnCloseRequest for primaryStage
@@ -35,11 +56,9 @@ public class Main extends Application {
                 }
                 Platform.exit();
             });
-
             primaryStage.show();
         } catch (Exception e) {
             ErrorAlert.getInstance().showAndWait(e);
         }
     }
-
 }
