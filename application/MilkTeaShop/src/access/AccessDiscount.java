dae89d3e2a2137181477681a402d04a447184ac5
ht0512 <dinyha2000@gmail.com>
2019-10-21 00:05:59 +0700

    update

--- application/MilkTeaShop/src/access/AccessDiscount.java
+++ application/MilkTeaShop/src/access/AccessDiscount.java
@@ -1,7 +1,7 @@
 package access;
 
-import controller.manager.Discount;
 import library.ErrorAlert;
+import model.Discount;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,7 +12,7 @@ import java.sql.SQLException;
  * The type Access discount.
  */
 public final class AccessDiscount {
-    private AccessDiscount instance;
+    private static AccessDiscount instance;
 
     @Contract(pure = true)
     private AccessDiscount() {
@@ -24,20 +24,22 @@ public final class AccessDiscount {
      * @return the instance
      */
     public static AccessDiscount getInstance() {
-        if(instance == null){
+        if (instance == null) {
             setInstance(new AccessDiscount());
         }
         return instance;
     }
 
-    private static void setInstance(AccessDiscount instance) { AccessDiscount.instance = instance; }
+    private static void setInstance(AccessDiscount instance) {
+        AccessDiscount.instance = instance;
+    }
 
     @Nullable
     public Discount insert(String name, double sale) {
-        try(ResultSet resultSet = DataProvider.getInstance().execute("exec InsertDiscount ?,?",new Object[]{name, sale});){
-        assert resultSet != null;
-        return resultSet.next() ? new Discount(resultSet): null;
-        }catch (SQLException e){
+        try (ResultSet resultSet = DataProvider.getInstance().execute("exec InsertDiscount ?,?", new Object[]{name, sale})) {
+            assert resultSet != null;
+            return resultSet.next() ? new Discount(resultSet) : null;
+        } catch (SQLException e) {
             ErrorAlert.getInstance().showAndWait(e);
             return null;
         }
