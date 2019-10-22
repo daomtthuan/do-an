1c60f8b79854c100d9ac0244e41fc662d689bd52
ht0512 <dinyha2000@gmail.com>
2019-10-22 10:37:47 +0700

    Update Discount.java

--- application/MilkTeaShop/src/controller/manager/Discount.java
+++ application/MilkTeaShop/src/controller/manager/Discount.java
@@ -1,8 +1,12 @@
 package controller.manager;
 
+import access.AccessDiscount;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import library.Tool;
+
+import static library.Tool.*;
 
 
 public final class Discount {
@@ -15,7 +19,11 @@ public final class Discount {
 
     @FXML
     private void submit(ActionEvent event){
-        StringBuilder name = new StringBuilder();
-      //  for()
+        String name = fixString(getName());
+        StringBuilder discountName = new StringBuilder();
+        for(String s : name.split("")){
+            discountName.append(s.charAt(0));
+        }
+        Discount discount = AccessDiscount.getInstance().insert(name, sale);
     }
 }
