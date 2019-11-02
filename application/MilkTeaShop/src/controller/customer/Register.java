911d58cbb0c46fdfa2557114ce8c67942d976965
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-11-03 04:36:04 +0700

    update

--- application/MilkTeaShop/src/controller/customer/Register.java
+++ application/MilkTeaShop/src/controller/customer/Register.java
@@ -8,47 +8,57 @@ import app.stage.SecondaryStage;
 import app.string.Regex;
 import app.string.Tool;
 import controller.Controller;
-import controller.general.WatchAccount;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.time.format.DateTimeFormatter;
 
 public final class Register implements Controller {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        setTitle("REGISTER");
-        SecondaryStage.getInstance().getStage().hide();
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private RadioButton maleRadioButton;
+    @FXML
+    private RadioButton femaleRadioButton;
+    @FXML
+    private DatePicker birthdayDatePicker;
+    @FXML
+    private TextField addressTextField;
+    @FXML
+    private TextField phoneTextField;
+    @FXML
+    private TextField emailTextField;
 
-        setDefaultButton("Submit");
-        getDefaultButton().setDefaultButton(true);
-        getDefaultButton().setOnAction(event -> {
-            String name = Tool.fixString(getName());
-            boolean gender = isMale();
-            String birthday = getBirthday();
-            String address = Tool.fixString(getAddress());
-            String phone = getPhone();
-            String email = getEmail().toLowerCase();
+    @FXML
+    private void submit() {
+        String name = Tool.fixString(nameTextField.getText());
+        boolean gender = maleRadioButton.isSelected();
+        String birthday = birthdayDatePicker.getValue() == null ? "" : birthdayDatePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String address = Tool.fixString(addressTextField.getText());
+        String phone = phoneTextField.getText();
+        String email = emailTextField.getText().toLowerCase();
 
-            if (name.matches(Regex.NAME) && address.matches(Regex.ADDRESS) && phone.matches(Regex.PHONE) && email.matches(Regex.EMAIL)) {
-                model.Account account = AccountApi.getInstance().insert(Tool.createAcronym(name), 1, name, gender, birthday, address, phone, email);
+        if (name.matches(Regex.NAME) && address.matches(Regex.ADDRESS) && phone.matches(Regex.PHONE) && email.matches(Regex.EMAIL) && (maleRadioButton.isSelected() || femaleRadioButton.isSelected())) {
+            model.Account account = AccountApi.getInstance().insert(Tool.createAcronym(name), 1, name, gender, birthday, address, phone, email);
 
-                if (account != null) {
-                    SecondaryStage.getInstance().setAccount(account);
+            if (account != null) {
+                SecondaryStage.getInstance().setAccount(account);
 
-                    if (!SecondaryStage.getInstance().isOrdering()) {
-                        SecondaryStage.getInstance().setScene("/view/customer/Order.fxml", "/style/customer/Order.css");
-                    }
-
-                    AlertInformation.getInstance().showAndWait("Success!",
-                            "Your Customer Account: " + account.getAccount() + "\nYour default Password: 1\nPlease change your Password at next Login times.\n" +
-                                    "Thanks for coming to our shop!");
-                    DialogSecondaryStage.getInstance().close();
-                } else {
-                    AlertWarning.getInstance().showAndWait("Fail!", "Can not register.\nPlease notify staff.");
+                if (!SecondaryStage.getInstance().isOrdering()) {
+                    SecondaryStage.getInstance().setScene("/view/customer/Order.fxml", "/style/customer/Order.css");
                 }
+
+                AlertInformation.getInstance().showAndWait("Success!",
+                        "Your Customer Account: " + account.getAccount() + "\nYour default Password: 1\nPlease change your Password at next Login times.\n" +
+                                "Thanks for coming to our shop!");
+                DialogSecondaryStage.getInstance().close();
             } else {
-                AlertWarning.getInstance().showAndWait("Fail!", "Invalid information.\nPlease check again.");
+                AlertWarning.getInstance().showAndWait("Fail!", "Can not register.\nPlease notify staff.");
             }
-        });
+        } else {
+            AlertWarning.getInstance().showAndWait("Fail!", "Invalid information.\nPlease check again.");
+        }
     }
 }
--- application/MilkTeaShop/src/controller/manager/EditAccount.java
+++ application/MilkTeaShop/src/controller/manager/EditAccount.java
@@ -9,20 +9,15 @@ import app.string.Regex;
 import app.string.Tool;
 import controller.Controller;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-import java.net.URL;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ResourceBundle;
 
-public class EditAccount implements Controller, Initializable {
-    @FXML
-    private VBox container;
-    @FXML
-    private Label titleLabel;
+public class EditAccount implements Controller {
     @FXML
     private TextField nameTextField;
     @FXML
@@ -37,123 +32,39 @@ public class EditAccount implements Controller, Initializable {
     private TextField phoneTextField;
     @FXML
     private TextField emailTextField;
-    @FXML
-    private Button defaultButton;
 
     private model.Account account;
 
-    private int getId() {
-        return account.getId();
-    }
-
-    public void setAccount(model.Account account) {
+    @Contract(pure = true)
+    public EditAccount(@NotNull model.Account account) {
         this.account = account;
     }
 
-    protected void setTitle(String titleLabel) {
-        this.titleLabel.setText(titleLabel);
-    }
+    @FXML
+    private void submit() {
+        int id = account.getId();
+        String name = Tool.fixString(nameTextField.getText());
+        boolean gender = maleRadioButton.isSelected();
+        String birthday = birthdayDatePicker.getValue() == null ? "" : birthdayDatePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String address = Tool.fixString(addressTextField.getText());
+        String phone = phoneTextField.getText();
+        String email = emailTextField.getText().toLowerCase();
+        if (name.matches(Regex.NAME) && address.matches(Regex.ADDRESS) && phone.matches(Regex.PHONE) && email.matches(Regex.EMAIL)) {
+            model.Account account = AccountApi.getInstance().update(id, name, gender, birthday, address, phone, email);
 
-    protected Button getDefaultButton() {
-        return defaultButton;
-    }
-
-    protected void setDefaultButton(String defaultButton) {
-        this.defaultButton.setText(defaultButton);
-    }
-
-    protected String getName() {
-        return nameTextField.getText();
-    }
-
-    protected boolean isMale() {
-        return maleRadioButton.isSelected();
-    }
-
-    protected String getBirthday() {
-        return birthdayDatePicker.getValue() == null ? "" : birthdayDatePicker.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-    }
-
-    protected String getAddress() {
-        return addressTextField.getText();
-    }
-
-    protected String getPhone() {
-        return phoneTextField.getText();
-    }
-
-    protected String getEmail() {
-        return emailTextField.getText();
-    }
-
-    private void edit() {
-        setTitle("EDIT INFORMATION");
-        setDefaultButton("Submit");
-        nameTextField.setDisable(false);
-        maleRadioButton.setDisable(false);
-        femaleRadioButton.setDisable(false);
-        birthdayDatePicker.setDisable(false);
-        addressTextField.setDisable(false);
-        phoneTextField.setDisable(false);
-        emailTextField.setDisable(false);
-        defaultButton.setOnAction(event -> {
-            String name = Tool.fixString(getName());
-            boolean gender = isMale();
-            String birthday = getBirthday();
-            String address = Tool.fixString(getAddress());
-            String phone = getPhone();
-            String email = getEmail().toLowerCase();
-            if (name.matches(Regex.NAME) && address.matches(Regex.ADDRESS) && phone.matches(Regex.PHONE) && email.matches(Regex.EMAIL)) {
-                model.Account account = AccountApi.getInstance().update(getId(), name, gender, birthday, address, phone, email);
-
-                if (account != null) {
-                    setAccount(account);
-                    if (account.getRoll() == 1) {
-                        SecondaryStage.getInstance().setAccount(account);
-                    } else {
-                        PrimaryStage.getInstance().setAccount(account);
-                    }
-                    AlertInformation.getInstance().showAndWait("Success!", "Your Information Account has been edited.");
-                    view();
+            if (account != null) {
+                setAccount(account);
+                if (account.getRoll() == 1) {
+                    SecondaryStage.getInstance().setAccount(account);
                 } else {
-                    AlertWarning.getInstance().showAndWait("Fail!", "Can not edit Account Information.\nPlease notify staff.");
+                    PrimaryStage.getInstance().setAccount(account);
                 }
+                AlertInformation.getInstance().showAndWait("Success!", "Your Information Account has been edited.");
             } else {
-                AlertWarning.getInstance().showAndWait("Fail!", "Invalid information.\nPlease check again.");
+                AlertWarning.getInstance().showAndWait("Fail!", "Can not edit Account Information.\nPlease notify staff.");
             }
-        });
-    }
-
-    private void view() {
-        setTitle("ACCOUNT INFORMATION");
-        setDefaultButton("Edit Information");
-        nameTextField.setDisable(true);
-        maleRadioButton.setDisable(true);
-        femaleRadioButton.setDisable(true);
-        birthdayDatePicker.setDisable(true);
-        addressTextField.setDisable(true);
-        phoneTextField.setDisable(true);
-        emailTextField.setDisable(true);
-
-        if (account.getRoll() == 3) {
-            defaultButton.setOnAction(event -> edit());
+        } else {
+            AlertWarning.getInstance().showAndWait("Fail!", "Invalid information.\nPlease check again.");
         }
-        else {
-            container.getChildren().remove(defaultButton);
-        }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        nameTextField.setText(account.getName());
-        maleRadioButton.setSelected(account.isMale());
-        femaleRadioButton.setSelected(account.isFemMale());
-        String[] elements = account.getBirthday().split("-");
-        birthdayDatePicker.setValue(LocalDate.of(Integer.parseInt(elements[0]), Integer.parseInt(elements[1]), Integer.parseInt(elements[2])));
-        addressTextField.setText(account.getAddress());
-        phoneTextField.setText(account.getPhone());
-        emailTextField.setText(account.getEmail());
-        getDefaultButton().setDefaultButton(true);
-        view();
     }
 }
