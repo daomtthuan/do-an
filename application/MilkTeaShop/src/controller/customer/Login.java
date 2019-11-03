25507ddc3572e098cfb24665293d545211adda14
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-11-03 07:42:20 +0700

    Update Login.java

--- application/MilkTeaShop/src/controller/customer/Login.java
+++ application/MilkTeaShop/src/controller/customer/Login.java
@@ -15,7 +15,6 @@ import java.util.ResourceBundle;
 public class Login extends controller.general.Login implements Controller, Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        getSubmitButton().setDefaultButton(true);
         getSubmitButton().setOnAction(actionEvent -> {
             if (getAccount().matches(Regex.ACCOUNT) && getPassword().matches(Regex.PASSWORD)) {
                 Account account = AccountApi.getInstance().login("Customer", getAccount(), getPassword());
