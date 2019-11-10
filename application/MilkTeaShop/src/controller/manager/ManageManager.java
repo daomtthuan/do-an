952aed16f2e7e29657032f59e6b2c72bfc80f7a4
Hoaiphongg <48853122+Hoaiphongg@users.noreply.github.com>
2019-11-10 15:40:00 +0700

    update

--- application/MilkTeaShop/src/controller/manager/ManageManager.java
+++ application/MilkTeaShop/src/controller/manager/ManageManager.java
@@ -2,20 +2,72 @@ package controller.manager;
 
 import app.pattern.Controller;
 import component.controller.ManagePane;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Account;
+
+import java.util.ArrayList;
 
 public class ManageManager extends ManagePane implements Controller {
+
+	@FXML
+	private TableView<Account> tableView;
+	private TableColumn<Account, Integer> idColumn;
+	private TableColumn<Account, String> accountColumn;
+	private TableColumn<Account, String> nameColumn;
+	private TableColumn<Account, String> genderColumn;
+	private TableColumn<Account, String> birthdayColumn;
+	private TableColumn<Account, String> addressColumn;
+	private TableColumn<Account, String> phoneColumn;
+	private TableColumn<Account, String> emailColumn;
+	private TableColumn<Account, String> statusColumn;
+	private ArrayList<Account> accounts;
+
 	ManageManager(Manager manager) {
 		super(manager);
+
+		idColumn = new TableColumn<>("Id Customer");
+		accountColumn = new TableColumn<>("Account");
+		nameColumn = new TableColumn<>("Name");
+		genderColumn = new TableColumn<>("Male");
+		birthdayColumn = new TableColumn<>("Birthday");
+		addressColumn = new TableColumn<>("Address");
+		phoneColumn = new TableColumn<>("Phone");
+		emailColumn = new TableColumn<>("Email");
+		statusColumn = new TableColumn<>("Enabled");
+
+		idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+		accountColumn.setCellValueFactory(new PropertyValueFactory<>("account"));
+		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		genderColumn.setCellValueFactory(new PropertyValueFactory<>("male"));
+		birthdayColumn.setCellValueFactory(new PropertyValueFactory<>("birthday"));
+		addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+		phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+		emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+		statusColumn.setCellValueFactory(new PropertyValueFactory<>("enabled"));
 	}
 
 	@Override
 	protected void setup() {
+		tableView.getColumns().add(idColumn);
+		tableView.getColumns().add(accountColumn);
+		tableView.getColumns().add(nameColumn);
+		tableView.getColumns().add(genderColumn);
+		tableView.getColumns().add(birthdayColumn);
+		tableView.getColumns().add(addressColumn);
+		tableView.getColumns().add(phoneColumn);
+		tableView.getColumns().add(emailColumn);
+		tableView.getColumns().add(statusColumn);
 
+		accounts = api.Account.getInstance().getAccounts(3);
 	}
 
 	@Override
 	protected void load() {
-
+		tableView.setItems(FXCollections.observableArrayList(accounts));
 	}
 
 	@Override
@@ -35,6 +87,7 @@ public class ManageManager extends ManagePane implements Controller {
 
 	@Override
 	protected void refresh() {
-
+		tableView.getItems().clear();
+		load();
 	}
 }
--- application/MilkTeaShop/src/controller/manager/ManageTable.java
+++ application/MilkTeaShop/src/controller/manager/ManageTable.java
@@ -2,20 +2,52 @@ package controller.manager;
 
 import app.pattern.Controller;
 import component.controller.ManagePane;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Table;
+
+import java.util.ArrayList;
 
 public class ManageTable extends ManagePane implements Controller {
+
+	@FXML
+	private TableView<Table> tableView;
+	private TableColumn<Table, Integer> idColumn;
+	private TableColumn<Table, Boolean> xColumn;
+	private TableColumn<Table, Boolean> yColumn;
+	private TableColumn<Table, Integer> statusColumn;
+	private ArrayList<Table> tables;
+
 	ManageTable(Manager manager) {
 		super(manager);
+		idColumn = new TableColumn<>("Id Table");
+		xColumn = new TableColumn<>("X");
+		yColumn = new TableColumn<>("Y");
+		statusColumn = new TableColumn<>("Status");
+
+		idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+		xColumn.setCellValueFactory(new PropertyValueFactory<>("x"));
+		yColumn.setCellValueFactory(new PropertyValueFactory<>("y"));
+		statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+
 	}
 
 	@Override
 	protected void setup() {
+		tableView.getColumns().add(idColumn);
+		tableView.getColumns().add(xColumn);
+		tableView.getColumns().add(yColumn);
+		tableView.getColumns().add(statusColumn);
 
+		tables = api.Table.getInstance().getTables();
 	}
 
 	@Override
 	protected void load() {
-
+		tableView.setItems(FXCollections.observableArrayList(tables));
 	}
 
 	@Override
@@ -35,6 +67,7 @@ public class ManageTable extends ManagePane implements Controller {
 
 	@Override
 	protected void refresh() {
-
+		tableView.getItems().clear();
+		load();
 	}
 }
