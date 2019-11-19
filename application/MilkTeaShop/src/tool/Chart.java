f9acacc2413103ec92e817bd7970ae6dc3ec43a7
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-11-20 02:58:49 +0700

    update

--- application/MilkTeaShop/src/tool/Chart.java
+++ application/MilkTeaShop/src/tool/Chart.java
@@ -1,58 +1,41 @@
 package tool;
 
-import javafx.application.Application;
+import app.alert.AlertWarning;
 import javafx.geometry.Side;
-import javafx.scene.Scene;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;
+import model.Income;
 
-public class Chart extends Application {
-	public static void main(String[] args) {
-		launch(args);
-	}
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-	@Override
-	public void start(Stage stage) {
-		final NumberAxis xAxis = new NumberAxis(1, 12, 1);
-		final NumberAxis yAxis = new NumberAxis();
-		final AreaChart<java.lang.Number, java.lang.Number> areaChart = new AreaChart<>(xAxis, yAxis);
-		areaChart.setTitle("Revenue");
+public class Chart {
+	public static AreaChart<java.lang.Number, java.lang.Number> create() {
+		LocalDate now = LocalDate.now();
+		AreaChart<java.lang.Number, java.lang.Number> areaChart = new AreaChart<>(new NumberAxis(1, now.getMonth().maxLength(), 1), new NumberAxis());
+		areaChart.setTitle("Income in month");
+		areaChart.setLegendSide(Side.TOP);
 
-		areaChart.setLegendSide(Side.LEFT);
+		XYChart.Series<java.lang.Number, java.lang.Number> thisMonthSeries = new XYChart.Series<>();
+		thisMonthSeries.setName("This month");
+		ArrayList<Income> thisMonthIncomes = api.Income.getInstance().getDayIncomes(now.getYear() + "-" + now.getMonthValue() + "-1", now.getYear() + "-" + now.getMonthValue() + "-" + now.getMonth().maxLength());
+		if (thisMonthIncomes != null) {
+			thisMonthIncomes.forEach(income -> thisMonthSeries.getData().add(new XYChart.Data<>(LocalDate.parse(income.getDate()).getDayOfMonth(), income.getIncome())));
+		} else {
+			AlertWarning.getInstance().showAndWait("Fail!", "Cannot calculate income.\nPlease notify staff.");
+		}
+		areaChart.getData().add(thisMonthSeries);
 
-		// Chuỗi dữ liệu của năm 2014
-		XYChart.Series<java.lang.Number, java.lang.Number> series2014 = new XYChart.Series<>();
-
-		series2014.setName("2014");
-
-		series2014.getData().add(new XYChart.Data<>(1, 400));
-		series2014.getData().add(new XYChart.Data<>(3, 1000));
-		series2014.getData().add(new XYChart.Data<>(4, 1500));
-		series2014.getData().add(new XYChart.Data<>(5, 800));
-		series2014.getData().add(new XYChart.Data<>(7, 500));
-		series2014.getData().add(new XYChart.Data<>(8, 1800));
-		series2014.getData().add(new XYChart.Data<>(10, 1500));
-		series2014.getData().add(new XYChart.Data<>(12, 1300));
-
-		// Chuỗi dữ liệu của năm 2015
-		XYChart.Series<java.lang.Number, java.lang.Number> series2015 = new XYChart.Series<>();
-		series2015.setName("2015");
-		series2015.getData().add(new XYChart.Data<>(1, 2000));
-		series2015.getData().add(new XYChart.Data<>(3, 1500));
-		series2015.getData().add(new XYChart.Data<>(4, 1300));
-		series2015.getData().add(new XYChart.Data<>(5, 1200));
-		series2015.getData().add(new XYChart.Data<>(7, 1400));
-		series2015.getData().add(new XYChart.Data<>(8, 1080));
-		series2015.getData().add(new XYChart.Data<>(10, 2050));
-		series2015.getData().add(new XYChart.Data<>(12, 2005));
-
-		stage.setTitle("AreaChart (o7planning.org)");
-		Scene scene = new Scene(areaChart, 400, 300);
-		areaChart.getData().add(series2014);
-		areaChart.getData().add(series2015);
-		stage.setScene(scene);
-		stage.show();
+		XYChart.Series<java.lang.Number, java.lang.Number> lastMonthSeries = new XYChart.Series<>();
+		lastMonthSeries.setName("Last month");
+		ArrayList<Income> lastMonthIncomes = api.Income.getInstance().getDayIncomes(now.getYear() + "-" + (now.getMonthValue() - 1) + "-1", now.getYear() + "-" + (now.getMonthValue() - 1) + "-" + now.getMonth().minus(1).maxLength());
+		if (lastMonthIncomes != null) {
+			lastMonthIncomes.forEach(income -> lastMonthSeries.getData().add(new XYChart.Data<>(LocalDate.parse(income.getDate()).getDayOfMonth(), income.getIncome())));
+		} else {
+			AlertWarning.getInstance().showAndWait("Fail!", "Cannot calculate income.\nPlease notify staff.");
+		}
+		areaChart.getData().add(lastMonthSeries);
+		return areaChart;
 	}
 }
